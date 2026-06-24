2e08f0a709d708c989cd70aa66e523ccb3c6aecf
Myahse <ahmanndesigning@gmail.com>
2026-06-24 15:24:32 +0000

    Fix proxy login 403 headers

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -171,6 +171,7 @@ export async function login(params: LoginParams): Promise<LoginResult> {
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest',
     },
     body: JSON.stringify(params),
   })
